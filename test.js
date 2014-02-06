2d1cc12a71593561087305b69b1be71c2efa1991
Evan Lucas <evanlucas@me.com>
2014-02-06 13:39:01 -0600

    Update test to listen to events

--- test.js
+++ test.js
@@ -1,7 +1,16 @@
-var parser = require('./lib')
+var Parser = require('./lib')
   , split = require('split')
 
+var parser = new Parser()
+
 process.stdin
-  .pipe(split(/\r|\n/))
-  .pipe(parser())
-  .pipe(process.stdout)
+  .pipe(split(/\r/))
+  .pipe(parser)
+
+parser.on('message', function(message) {
+  console.log('Message:', message)
+})
+
+parser.on('segment', function(segment) {
+  console.log('Segment:', segment)
+})
