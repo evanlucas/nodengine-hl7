f5a7aee922b35faa8a5938cd3a6540512b216f76
Evan Lucas <evanlucas@me.com>
2014-02-06 18:14:23 -0600

    Added additional check to Message tests

--- test/message.js
+++ test/message.js
@@ -4,11 +4,11 @@ var should = require('should')
 describe('Message', function() {
   it('should support using new', function() {
     var m = new Message()
+    m.should.be.instanceOf(Message)
   })
 
   it('should support not using new', function() {
     var m = Message()
+    m.should.be.instanceOf(Message)
   })
-
-
 })
